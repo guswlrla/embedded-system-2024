8c9fb09de14e14abde00dfe911ddf00caaed25f8
guswlrla <158007465+guswlrla@users.noreply.github.com>
2024-03-15 17:20:18 +0900

    update

---
+++ day02/test02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define BUFSIZE 14 // null 문자가 들어갈 공간을 줘야함(+1)
+
+void main()
+{
+  char str[BUFSIZE] = "RaspberryPi!!";
+  printf("%s\n", str);
+
+  for(int i = 0; i < BUFSIZE; i++)
+  {
+    printf("%c", str[i]);
+  }
+  printf("\n");
+}
+
