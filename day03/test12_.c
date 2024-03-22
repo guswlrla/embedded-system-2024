88d07da404a030f8f935410dab3cf3d17993a4bf
guswlrla <rlaguswl_01@naver.com>
2024-03-22 17:27:44 +0900

    update

--- day03/test12_.c
+++ day03/test12_.c
@@ -2,13 +2,13 @@
 
 void main()
 {
-char ch, ch2;
-int n;
-// printf("문자: ");
-// scanf("%c", &ch);
-printf("학번: ");
-scanf("%d", &n);
-printf("이름:  ");
-scanf("%c", &ch);
-printf("%d, %c\n", ch, n);
+    char ch, ch2;
+    int n;
+    // printf("문자: ");
+    // scanf("%c", &ch);
+    printf("학번: ");
+    scanf("%d", &n);
+    printf("이름:  ");
+    scanf("%c", &ch);
+    printf("%d, %c\n", ch, n);
 }
